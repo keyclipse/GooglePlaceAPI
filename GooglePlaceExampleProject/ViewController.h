2290480a9086c6723fa78d84fb7d827130591b6a
Samuel Kitono <sdkitono@gmail.com>
2012-03-14 17:15:18 +1100

    Initial Commit

---
+++ GooglePlaceExampleProject/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  GooglePlaceExampleProject
+//
+//  Created by Samuel Kitono on 14/03/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
